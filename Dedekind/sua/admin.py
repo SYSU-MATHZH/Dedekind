957001be1abbdae267282dc62b8717a5d4ccd168
Nouzan.Ubu <clobraindie@outlook.com>
2017-07-11 22:01:18 +0800

    添加Student模型;修改Sua模型字段名

--- Dedekind/sua/admin.py
+++ Dedekind/sua/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Sua
+from .models import Sua, Student
 
 
 admin.site.register(Sua)
+admin.site.register(Student)
--- Dedekind/sua/models.py
+++ Dedekind/sua/models.py
@@ -1,13 +1,36 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+from django.utils.translation import ugettext as _
+import datetime
+
+
+YEAR_CHOICES = []
+for r in range(1980, (datetime.datetime.now().year+4)):
+    YEAR_CHOICES.append((r, r))
+
+
+class Student(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    number = models.IntegerField(_("Student Number"))
+    suahours = models.FloatField()
+    name = models.CharField(max_length=100)
+    grade = models.IntegerField(_("Student Grade"), choices=YEAR_CHOICES, default=datetime.datetime.now().year)
+
+    def __str__(self):
+        return self.name
 
 
 class Sua(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    sua_title = models.CharField(max_length=200)
-    sua_team = models.CharField(max_length=200)
-    sua_date = models.DateTimeField('活动日期')
-    sua_hours = models.FloatField()
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    team = models.CharField(max_length=200)
+    date = models.DateTimeField('活动日期')
+    suahours = models.FloatField()
 
     def __str__(self):
-        return self.sua_title
+        return self.title
