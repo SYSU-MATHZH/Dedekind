2933edc4182b93cafada18abe085a4e7a94ac0ed
Nouzan.Ubu <clobraindie@outlook.com>
2017-07-12 21:18:52 +0800

    增加删除Student时也删除User，删除Sua_Application时也删除Sua，删除Sua_Application也删除已无其他关联Sua_Application的非线下Proof

--- Dedekind/sua/signals/handlers.py
+++ Dedekind/sua/signals/handlers.py
@@ -1,6 +1,6 @@
-from django.db.models.signals import pre_save, pre_delete
+from django.db.models.signals import pre_save, pre_delete, post_delete
 from django.dispatch import receiver
-from sua.models import Sua
+from sua.models import Sua, Student, Sua_Application
 
 
 @receiver(pre_save, sender=Sua, dispatch_uid="Sua_pre_save")
@@ -19,3 +19,23 @@ def Sua_pre_delete_handler(sender, **kwargs):
     if sua.is_valid:
         sua.suahours = 0.0
         sua.update_student_suahours()
+
+
+@receiver(post_delete, sender=Student, dispatch_uid="Student_post_delete")
+def Student_post_delete_handler(sender, instance, *args, **kwargs):
+    if instance.user:  # just in case user is not specified
+        instance.user.delete()
+
+
+@receiver(
+    post_delete,
+    sender=Sua_Application,
+    dispatch_uid="Sua_Application_post_delete",
+)
+def Sua_Applicationpost_delete_handler(sender, instance, *args, **kwargs):
+    if instance.sua:  # just in case user is not specified
+        instance.sua.delete()
+    if instance.proof:
+        pf = instance.proof
+        if pf.sua_application_set.count() == 0 and not pf.is_offline:
+            instance.proof.delete()
