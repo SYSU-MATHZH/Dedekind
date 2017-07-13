3d3ede4e57b32f3ad45e9f68449cdfda14d6ab90
Nouzan.Ubu <clobraindie@outlook.com>
2017-07-13 16:13:29 +0800

    添加静态文件直接访问

--- Dedekind/Dedekind/settings.py
+++ Dedekind/Dedekind/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from .key import MY_SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k0%%x5el=bh@l-$q(_pc-s$yf7f*=@z3r%zf)c#5y3z9t+r%pf'
+SECRET_KEY = MY_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
